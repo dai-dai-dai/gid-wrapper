1ca12f9963cde3a13281b7a9e4d5b3ae7da1a04b
Anna Dai <anna_dai@brown.edu>
2023-03-20 13:39:41 -0400

    x;

---
+++ gid.go
@@ -0,0 +1,30 @@
+package gid
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func SetGid() {
+	// get the current process id
+	_ = os.Getpid()
+
+	_ = syscall.Getgid()
+
+	// set the new gid to 1101
+	err := syscall.Setgid(1101)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// verify that the gid has been updated
+	newGid := syscall.Getgid()
+	if newGid != 1101 {
+		fmt.Println("Error: gid not updated")
+		return
+	}
+
+	fmt.Println("GID updated successfully!")
+}
